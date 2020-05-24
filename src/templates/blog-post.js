8ba90e13ac75402f4256a32f67bd252098fa7822
islam <islaam.agamy@gmail.com>
2020-05-25 01:21:09 +0200

    ,l

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,9 +5,6 @@ import { Helmet } from "react-helmet";
 import { graphql, Link } from "gatsby";
 import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
-import Img from "gatsby-image";
-import ShareButtons from "../components/ShareButtons";
-import useSiteMetadata from "../components/SiteMetadata";
 
 export const BlogPostTemplate = ({
   content,
@@ -15,59 +12,37 @@ export const BlogPostTemplate = ({
   description,
   tags,
   title,
-  date,
   helmet,
-  featuredImage,
-  slug,
 }) => {
   const PostContent = contentComponent || Content;
-  const siteMetadata = useSiteMetadata();
 
   return (
-    <>
-      <div className="spacer" />
-      <section className="section">
-        {helmet || ""}
-        <div className="container content">
-          <div className="columns" style={{ margin: "0px" }}>
-            <div style={{ maxWidth: "1160px", width: "100%" }}>
-              <div className="post-header">
-                <div className="post-header__left">
-                  <h1 className="title has-text-weight-bold is-bold-light">
-                    {title}
-                  </h1>
-                  <p className="post-description">{description}</p>
-                  <p className="post-date">{date}</p>
-                </div>
-                <div className="post-header__right">
-                  <Img fluid={featuredImage} className="featured-image" />
-                </div>
+    <section className="section">
+      {helmet || ""}
+      <div className="container content">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+              {title}
+            </h1>
+            <p>{description}</p>
+            <PostContent content={content} />
+            {tags && tags.length ? (
+              <div style={{ marginTop: `4rem` }}>
+                <h4>Tags</h4>
+                <ul className="taglist">
+                  {tags.map((tag) => (
+                    <li key={tag + `tag`}>
+                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                    </li>
+                  ))}
+                </ul>
               </div>
-              <div style={{ position: "relative", marginTop: "60px" }}>
-                <ShareButtons
-                  tags={tags}
-                  title={title}
-                  url={`${siteMetadata.siteUrl}${slug}`}
-                />
-                <PostContent className="post-content" content={content} />
-              </div>
-              {tags && tags.length ? (
-                <div style={{ marginTop: `4rem` }}>
-                  <h4>Tags</h4>
-                  <ul className="taglist">
-                    {tags.map((tag) => (
-                      <li key={tag + `tag`}>
-                        <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ) : null}
-            </div>
+            ) : null}
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
@@ -81,9 +56,6 @@ BlogPostTemplate.propTypes = {
 
 const BlogPost = ({ data }) => {
   const { markdownRemark: post } = data;
-  const image = post.frontmatter.featuredimage
-    ? post.frontmatter.featuredimage.childImageSharp.fluid
-    : null;
 
   return (
     <Layout>
@@ -91,9 +63,6 @@ const BlogPost = ({ data }) => {
         content={post.html}
         contentComponent={HTMLContent}
         description={post.frontmatter.description}
-        date={post.frontmatter.date}
-        featuredImage={image}
-        featuredpost={post.frontmatter.featuredpost}
         helmet={
           <Helmet titleTemplate="%s | Blog">
             <title>{`${post.frontmatter.title}`}</title>
@@ -101,12 +70,10 @@ const BlogPost = ({ data }) => {
               name="description"
               content={`${post.frontmatter.description}`}
             />
-            <meta property="og:image" content={`${image}`} />
           </Helmet>
         }
         tags={post.frontmatter.tags}
         title={post.frontmatter.title}
-        slug={post.fields.slug}
       />
     </Layout>
   );
@@ -130,17 +97,6 @@ export const pageQuery = graphql`
         title
         description
         tags
-        featuredimage {
-          childImageSharp {
-            fluid(maxWidth: 800) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        featuredpost
-      }
-      fields {
-        slug
       }
     }
   }
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,133 +1,109 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
+import facebook from "../img/social/facebook.svg";
+import instagram from "../img/social/instagram.svg";
+import twitter from "../img/social/twitter.svg";
+import vimeo from "../img/social/vimeo.svg";
 
-import Layout from '../components/Layout'
-import Features from '../components/Features'
-import BlogRoll from '../components/BlogRoll'
+import Layout from "../components/Layout";
+import BlogRoll from "../components/BlogRoll";
 
-export const IndexPageTemplate = ({
-  image,
-  title,
-  heading,
-  subheading,
-  mainpitch,
-  description,
-  intro,
-}) => (
+export const IndexPageTemplate = ({ image, title, heading, subheading }) => (
   <div>
     <div
       className="full-width-image margin-top-0"
       style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-        backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
+        display: "flex",
       }}
     >
-      <div
-        style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column',
-        }}
-      >
-        <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
+      <div className="header-text">
+        <h1 className="main-header is-size-3-mobile is-size-2-tablet is-size-1-widescreen">
           {title}
         </h1>
-        <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
+        <hr className="hero-horizontal" />
+        <h3 className="sub-header has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen">
           {subheading}
         </h3>
+        <div
+          className="social-header social footer-column"
+          style={{ padding: "0px" }}
+        >
+          <div className="social-icons social-icons-header">
+            <a
+              title="facebook"
+              href="https://www.facebook.com/SoUsHiiiii?ref=br_rs"
+            >
+              <img
+                src={facebook}
+                alt="Facebook"
+                style={{ width: "1em", height: "1em" }}
+              />
+            </a>
+            <a title="twitter" href="https://twitter.com">
+              <img
+                className="fas fa-lg"
+                src={twitter}
+                alt="Twitter"
+                style={{ width: "1em", height: "1em" }}
+              />
+            </a>
+            <a title="instagram" href="https://instagram.com">
+              <img
+                src={instagram}
+                alt="Instagram"
+                style={{ width: "1em", height: "1em" }}
+              />
+            </a>
+            <a title="vimeo" href="https://vimeo.com">
+              <img
+                src={vimeo}
+                alt="Vimeo"
+                style={{ width: "1em", height: "1em" }}
+              />
+            </a>
+          </div>
+        </div>
       </div>
+      <div
+        className="hero-img"
+        style={{
+          backgroundImage: `url(${
+            !!image.childImageSharp ? image.childImageSharp.fluid.src : image
+          })`,
+        }}
+      />
     </div>
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="section">
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div className="content">
-                  <div className="tile">
-                    <h1 className="title">{mainpitch.title}</h1>
-                  </div>
-                  <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
-                  </div>
-                </div>
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      {heading}
-                    </h3>
-                    <p>{description}</p>
-                  </div>
-                </div>
-                <Features gridItems={intro.blurbs} />
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
-                </div>
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
-                  </h3>
-                  <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
-                  </div>
-                </div>
-              </div>
+    <section className="index-section section section--gradient">
+      <div className="columns">
+        <div className="content">
+          <div className="column is-12" style={{ padding: "0" }}>
+            <h3 className="has-text-weight-semibold is-size-2">
+              Latest stories
+            </h3>
+            <BlogRoll />
+            <div className="column is-12 has-text-centered">
+              <Link className="btn" to="/blog">
+                Read more
+              </Link>
             </div>
           </div>
         </div>
       </div>
     </section>
   </div>
-)
+);
 
 IndexPageTemplate.propTypes = {
   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   title: PropTypes.string,
   heading: PropTypes.string,
   subheading: PropTypes.string,
-  mainpitch: PropTypes.object,
   description: PropTypes.string,
-  intro: PropTypes.shape({
-    blurbs: PropTypes.array,
-  }),
-}
+};
 
 const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
+  const { frontmatter } = data.markdownRemark;
 
   return (
     <Layout>
@@ -136,13 +112,11 @@ const IndexPage = ({ data }) => {
         title={frontmatter.title}
         heading={frontmatter.heading}
         subheading={frontmatter.subheading}
-        mainpitch={frontmatter.mainpitch}
         description={frontmatter.description}
-        intro={frontmatter.intro}
       />
     </Layout>
-  )
-}
+  );
+};
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
@@ -150,15 +124,18 @@ IndexPage.propTypes = {
       frontmatter: PropTypes.object,
     }),
   }),
-}
+};
 
-export default IndexPage
+export default IndexPage;
 
 export const pageQuery = graphql`
   query IndexPageTemplate {
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
         title
+        heading
+        subheading
+        description
         image {
           childImageSharp {
             fluid(maxWidth: 2048, quality: 100) {
@@ -166,28 +143,7 @@ export const pageQuery = graphql`
             }
           }
         }
-        heading
-        subheading
-        mainpitch {
-          title
-          description
-        }
-        description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-            text
-          }
-          heading
-          description
-        }
       }
     }
   }
-`
+`;
